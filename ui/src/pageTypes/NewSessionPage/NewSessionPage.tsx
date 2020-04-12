fa29cfac4a6b4530d79504f3bf1d1feb7c7a9212
Stefan Miodrag <stefanmiodrag@gmail.com>
2020-04-12 17:13:43 +0200

    Update NewSessionPage

--- ui/src/pageTypes/NewSessionPage/NewSessionPage.tsx
+++ ui/src/pageTypes/NewSessionPage/NewSessionPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Button, Container, Card } from '../../components';
 
+import './NewSessionPage.scss';
+
 interface INewSessionPage {
     handleNewSessionOnClick: any,
 }
@@ -11,11 +13,20 @@ const NewSessionPage = (props: INewSessionPage) => {
 
     return (
         <Container>
-            <Card width="600px">
-                <form onSubmit={handleNewSessionOnClick}>
-                    <Button label="New session" handleClick={handleNewSessionOnClick} />
-                </form>
-            </Card>
+            <div className="p-new-session">
+                <div className="p-new-session__heading">
+                    <span className="c-label">Currently in Beta</span>
+
+                    <h1>A curated shopping experience with your friend.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+
+                <Card width="600px">
+                    <form onSubmit={handleNewSessionOnClick}>
+                        <Button label="New session" handleClick={handleNewSessionOnClick} />
+                    </form>
+                </Card>
+            </div>
         </Container>
     )
 }
