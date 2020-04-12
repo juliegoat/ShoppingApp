b3f7b8e0e292a4aec6dffb9aec95087a833797a3
Stefan Miodrag <stefanmiodrag@gmail.com>
2020-04-12 16:57:14 +0200

    Remove unused Video partial

--- ui/src/partials/index.ts
+++ ui/src/partials/index.ts
@@ -1,11 +1,9 @@
 import Navigation from './Navigation';
 import StoreFront from './StoreFront';
 import Cart from './Cart';
-import Video from './Video';
 
 export {
     Navigation,
     StoreFront,
     Cart,
-    Video
 }
