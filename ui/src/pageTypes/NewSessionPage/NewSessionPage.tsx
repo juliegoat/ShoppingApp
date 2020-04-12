653104b253dda645529932a57fd8374190cb634e
Stefan Miodrag <stefanmiodrag@gmail.com>
2020-04-12 18:36:54 +0200

    Update buttons

--- ui/src/pageTypes/NewSessionPage/NewSessionPage.tsx
+++ ui/src/pageTypes/NewSessionPage/NewSessionPage.tsx
@@ -18,33 +18,40 @@ const NewSessionPage = (props: INewSessionPage) => {
                     <span className="c-label">Currently in Beta</span>
 
                     <h1>A curated shopping experience with your friend.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
                 <Card width="600px">
-                    <div>
-                        <h2 className="form-header">What are you shopping for?</h2>
-                        <button>Sweaters   </button>
-                        <button>Candles   </button>
-                        <button>Make-up   </button>
-                        <button>Snacks   </button>
-                        <button>Toilet Paper   </button>
-                        <button>Just browsing!   </button>
+                    <div className="p-new-session__buttons">
+                        <div style={{ marginBottom: "15px" }}>
+                            <strong>What are you shopping for?</strong>
+                            <Button label="Sweaters" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Candles" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Make-up" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Snacks" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Toilet Paper" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Just browsing!" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                        </div>
 
-                        <h2 className="form-header">What kind of shops do you like?</h2>
-                        <button>Local to me   </button>
-                        <button>Women-owned   </button>
-                        <button>Sustainably-sourced   </button>
-                        <button>No Preference!   </button>
+                        <div style={{ marginBottom: "15px" }}>
+                            <strong>What kind of shops do you like?</strong>
+                            <Button label="Local to me" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Women-owned" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="Sustainably-sourced" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="No Preference!" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                        </div>
 
-                        <h2 className="form-header">What kind of price range are you looking at?</h2>
-                        <button>$   </button>
-                        <button>$$   </button>
-                        <button>$$$   </button>
-                        <button>$$$$   </button>
-                        <button>I'll worry about that later!   </button>
-
+                        <div style={{ marginBottom: "15px" }}>
+                            <strong>What kind of price range are you looking at?</strong>
+                            <Button label="$" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="$$" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="$$$" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="$$$$" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                            <Button label="I'll worry about that later!" bg="transparent" color="#1D1D2E" border="1px solid #1D1D2E" />
+                        </div>
                     </div>
+
                     <form onSubmit={handleNewSessionOnClick}>
                         <Button label="Start shopping" handleClick={handleNewSessionOnClick} />
                     </form>
